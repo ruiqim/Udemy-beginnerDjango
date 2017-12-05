87840faf21bf1d8461960ad6ddd9bd65b91ab28c
Ruiqi Mao <iamruiqi@gmail.com>
2017-12-04 22:08:56 -0500

    added urls and views for tranaslate; refactored hello to home

--- piglatin/views.py
+++ piglatin/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def hello(request):
+def home(request):
     return render(request, 'home.html')
+
+
+def translate(request):
+    return HttpResponse("You're on the translate page")
